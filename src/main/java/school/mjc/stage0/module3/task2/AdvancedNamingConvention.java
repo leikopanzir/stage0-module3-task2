a9c602a8f3dc1502e34db15633409d860c5af440
leikopanzir <125738409+leikopanzir@users.noreply.github.com>
2024-06-22 16:10:50 +0300

    Update AdvancedNamingConvention.java

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -3,8 +3,8 @@ package school.mjc.stage0.module3.task2;
 public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
-    private int age;
-    private int phoneNumber;
+    private int age = 10;
+    private int phoneNumber = 1825;
 
     void callToFriend() {
         //some code
