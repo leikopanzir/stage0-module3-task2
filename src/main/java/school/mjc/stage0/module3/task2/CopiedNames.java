bedb8a23028979967f94e942e073a4e41cac0c52
leikopanzir <125738409+leikopanzir@users.noreply.github.com>
2024-06-22 16:33:55 +0300

    Update CopiedNames.java

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package school.mjc.stage0.module3.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variableSecond = 2000;
-        int value = 1000;
-        int variableThree = 4000;
-        int valueSecond = 3000;
-        int variables = 5000;
-        int variablesSeven = 7000;
-        int valueNine = 90000;
+        int first = 0;
+        int secondTwo = 2000;
+        int valuev = 1000;
+        int three = 4000;
+        int second = 3000;
+        int bles = 5000;
+        int seven = 7000;
+        int nine = 90000;
     }
 }
